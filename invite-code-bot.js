ab648997472a877932e010e6d47c12c762bc73d0
funny-snek <42410961+funny-snek@users.noreply.github.com>
2018-11-12 17:07:26 -0600

    added notes

--- invite-code-bot.js
+++ invite-code-bot.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = 'Your Discord Token Here'
+const TOKEN = 'Your Discord Token Here' /*change 'Your Discord Token Here to the token of your bot*/
+
+//notes
+/*bot will listen to whatever channel it has access to. Will send private message with the current code*/
+/*anything with quotes ' ' information goes inside them*/
+
+
+
 
 
 bot.on('message', function(message)
@@ -9,9 +16,11 @@ bot.on('message', function(message)
     {
     message.author.send("Nice Try. Please use the proper channel."); 
     }
-    else if(message.content == '!code')
+    else if(message.content == '!code') /*change '!code' to whatever you would like people to have to say to trigger the bot */
     {
         var fs = require('fs');
+        /*change 'C://Program Files (x86)/Steam/steamapps/common/Stardew Valley/Mods/Always On Server/InviteCode.txt' 
+below to wherevere yours is stored*/
         fs.readFile('C://Program Files (x86)/Steam/steamapps/common/Stardew Valley/Mods/Always On Server/InviteCode.txt', 'utf8', function(err, data) {  
             if (err) throw err;
             console.log(data);
